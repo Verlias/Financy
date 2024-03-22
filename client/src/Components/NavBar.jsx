41fbaa267fa4d282548e081068e8c99c4ec5197a
Logan <loganvoravong@gmail.com>
2024-03-21 20:57:45 -0400

    updated navbar minor fix

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -6,17 +6,19 @@ import styles from "./NavBar.module.css";
 
 function NavBar(){
     return(
-        <nav className={styles.nav}>
-            <Link to="/" className={styles.site_name}>Financy</Link>
-            <ul>
-                <li className={styles.active}>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className={styles.nav}>
+                <Link to="/" className={styles.site_name}>Financy</Link>
+                <ul>
+                    <li className={styles.active}>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
